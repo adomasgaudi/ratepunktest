d4a88dc980b9f1b1412c82073b320940a4d6e51d
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2024-02-27 14:34:50 +0200

    feat: 4 sections empty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 
 import st from './page.module.sass'
+import { Footer, NavBar, SectionMain, SectionStore } from './sections';
 
 export default function Home() {
   return (
     <main className={st.test}>
+      <NavBar></NavBar>
+      <SectionMain></SectionMain>
+      <SectionStore></SectionStore>
+      <Footer></Footer>
     </main>
   );
 }
---
+++ src/app/sections/index.tsx
@@ -0,0 +1,28 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <h1>navb=</h1>
+    </nav>
+  );
+};
+export const SectionMain = () => {
+  return (
+    <nav>
+      <h1>section main</h1>
+    </nav>
+  );
+};
+export const SectionStore = () => {
+  return (
+    <nav>
+      <h1>navb=</h1>
+    </nav>
+  );
+};
+export const Footer = () => {
+  return (
+    <nav>
+      <h1>navb=</h1>
+    </nav>
+  );
+};
