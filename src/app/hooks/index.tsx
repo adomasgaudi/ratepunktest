18d0ebd7099128e67f35f8047b32e218698d2f70
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2024-02-29 11:27:15 +0200

    fix: build, min height

--- src/app/hooks/index.tsx
+++ src/app/hooks/index.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 
 export const useIsMobile = (breakpoint: number) => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
-  
-    useEffect(() => {
+  const [isMobile, setIsMobile] = useState(
+    typeof window !== "undefined" && window.innerWidth < breakpoint
+  );
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
       const handleResize = () => {
         setIsMobile(window.innerWidth < breakpoint);
       };
-  
+
       window.addEventListener("resize", handleResize);
-  
+
       return () => {
         window.removeEventListener("resize", handleResize);
       };
-    }, [breakpoint]);
-  
-    return isMobile;
-  };
+    }
+  }, [breakpoint]);
+
+  return isMobile;
+};
